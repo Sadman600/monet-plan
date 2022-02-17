54671e4a953e983ba6159e504ebd5c8a7812d9b8
Sadman600 <sadmansakib600@gmail.com>
2022-02-17 22:10:32 +0600

    Added main section and added event handler

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
 
-console.log('money plan');
+function getIncome() {
+    const inputMoney = document.getElementById('income-money');
+    const incomeMoney = parseFloat(inputMoney.value);
+    return incomeMoney;
+}
+
+function getTotalCost() {
+    const foodCost = document.getElementById('food-cost');
+    const rentCost = document.getElementById('rent-cost');
+    const clothesCost = document.getElementById('clothes-cost');
+    const totalCost = parseFloat(foodCost.value) + parseFloat(rentCost.value) + parseFloat(clothesCost.value);
+    return totalCost;
+}
+
+document.getElementById('expenses-btn').addEventListener('click', function () {
+    const totalIncome = getIncome();
+    const totalCost = getTotalCost();
+    const totalExpenses = document.getElementById('total-expenses');
+    if (isNaN(totalCost)) {
+        alert('plz input number');
+        totalBalance.innerText = 0;
+    } else {
+        totalExpenses.innerText = totalCost;
+    }
+
+    const totalBalance = document.getElementById('total-balance');
+    if (totalCost > totalIncome) {
+        alert('very big cost');
+    } else {
+        totalBalance.innerText = totalIncome - totalCost;
+    }
+
+});
+
+
+
